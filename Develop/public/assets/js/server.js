e1e493cd385caf0413208132fcb1aa61a37f5770
Heather Smith <halexsmith86@gmail.com>
2021-03-01 15:49:12 -0500

    Adding server.js, basic server setup

---
+++ Develop/public/assets/js/server.js
@@ -0,0 +1,18 @@
+// dependencies
+const fs = require('fs');
+const express = require('express');
+const path = require('path');
+
+// sets up Express
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// designated port
+const PORT = 8080;
+
+// route to the main page - untested
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
+
+// route to the notes page - untested
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
