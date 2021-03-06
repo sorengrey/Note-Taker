38307817148c3afde623246488c2cf83744f84b5
Heather Smith <halexsmith86@gmail.com>
2021-03-06 17:52:37 -0500

    Removed console logs

--- Develop/public/assets/routes/apiRoutes.js
+++ Develop/public/assets/routes/apiRoutes.js
@@ -4,49 +4,29 @@ const path = require("path");
 
 // exports the routes
 module.exports = (app) => {
-    // get route for notes page 
-    app.get('/api/notes', (req, res) => {
-      // console.log(noteObject);
-      fs.readFile('./Develop/db/db.json', 'utf8', (err, data) => {
-        if (err) throw err;
-        //console.log(data);
-        res.send(data);
-        // const noteObject = JSON.parse(data);
-       // console.log(noteObject);
-      })
-  
-     // console.log(noteObject);
-    })
 
-    //post route for the notes page
-    app.post('/api/notes', (req, res) => {
-      let note = req.body;
- 
-      fs.readFile('./Develop/db/db.json', 'utf8', (err, data) => {
+  // get route for notes page 
+  app.get('/api/notes', (req, res) => {
+    fs.readFile('./Develop/db/db.json', 'utf8', (err, data) => {
+      if (err) throw err;
+      res.send(data);
+    })
+  })
+
+  //post route for the notes page
+  app.post('/api/notes', (req, res) => {
+    let note = req.body;
+    // reads the json file
+    fs.readFile('./Develop/db/db.json', 'utf8', (err, data) => {
+      if (err) throw err;
+      res.send(data);
+      const noteObject = JSON.parse(data);
+      noteObject.push(note);
+
+      //writes what's in the json to the page
+      fs.writeFile('./Develop/db/db.json', JSON.stringify(noteObject), err => {
         if (err) throw err;
-        // console.log(data);
-        res.send(data);
-        const noteObject = JSON.parse(data);
-       // console.log(noteObject);
-        // console.log(typeof noteObject);
-        // console.log(noteObject);
-        noteObject.push(note);
-        console.log(noteObject);
-        fs.writeFile('./Develop/db/db.json', JSON.stringify(noteObject), err => {
-          if (err) throw err;
-          return true;
+        return true;
       });
-      })
-      // console.log(data);
-     
     })
-
-    // writes to db.json
-    // const writeNotes = () => {
-    //   fs.writeFile('./Develop/db/db.json', JSON.stringify(), err => {
-    //     if (err) throw err;
-    //     return true;
-    // });
-    // }
-
-  }
+  })}
--- Develop/public/assets/routes/htmlRoutes.js
+++ Develop/public/assets/routes/htmlRoutes.js
@@ -13,8 +13,8 @@ module.exports = (app) => {
         res.sendFile(path.join(__dirname, '../../notes.html'));
     });
 
-      // if no matching route is found default to home
-  app.get('*', (req, res) => {
+    // if no matching route is found default to home
+    app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/Develop/public/index.html'));
   });
 }
